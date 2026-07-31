783cfdc1746ec7406321d53bbd8ec934c2dff6fc
ponnadachakravarthi219@gmail.com <ponnadachakravarthi219@gmail.com>
2026-07-31 19:53:17 +0530

    updating files

---
+++ variables/var.py
@@ -0,0 +1,30 @@
+# ========================================#
+#       Basic of variables codes
+# ========================================#
+# varable stores the data values 
+# sting = name 
+# int = numbers 
+# float = 5.9
+# boolean = true,false
+name = "KING"
+age = 19
+height = 5.0
+is_student = True
+
+print(name)
+
+print(height)
+print(is_student)
+
+# change the age 
+age = 21
+print(age)
+# multiple variables 
+X,Y,Z = 1,2,4
+print(X,Y,Z)
+# changing variable 
+score = 10 
+score = 20 
+print(score)
+
+# 
