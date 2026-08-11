46bbdcfcd7bfa1ebe0f338c67764baee77d1607d
ponnadachakravarthi219@gmail.com <ponnadachakravarthi219@gmail.com>
2026-08-11 07:00:53 +0530

    update phase 7

---
+++ Tcsnqt/1to10.py
@@ -0,0 +1,43 @@
+# #check Even or Odd
+# n = int(input("Enter A number: "))
+
+# if n % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")    
+
+# #check prime number   
+# n = int(input("Enter a Prime number: "))
+# if n <= 1:
+#     print("not a prime number")
+# else:
+#     for i in range(2, n):
+#         if n % i == 0:
+
+#             print("not a prime number") 
+#             break 
+#     else:
+#         print("prime number")     
+
+
+# #factorial number 
+# n = int(input("Enter a factorial  number: "))
+# fact = 1
+# for i in range (1,n+1):
+#     fact = fact * i
+# print("Factorial=", fact) 
+
+#reverse string 
+n = input("Enter a Reverse number: ")
+reverse =""
+for i in n:
+    reverse = i + reverse
+
+print("Reverse=",reverse)
+
+
+
+  
+
+
+ 
